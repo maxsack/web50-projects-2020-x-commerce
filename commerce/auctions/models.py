bb616ba42b37be2537fa41f217e170aaaa147aed
Tina Sack <t.ks@posteo.de>
2022-10-28 18:02:00 +0200

    update models for watchlist

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -19,6 +19,7 @@ class Listing(models.Model):
     isActive = models.BooleanField(default=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name="user")
     category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name="category")
+    watchlist = models.ManyToManyField(User, blank=True, null=True, related_name="watchlistListing")
 
     def __str__(self):
         return self.title
