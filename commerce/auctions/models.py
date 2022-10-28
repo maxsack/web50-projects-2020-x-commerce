a215c2b56cca079e541273bca9c3418dc141d3e9
Tina Sack <t.ks@posteo.de>
2022-10-28 11:04:03 +0200

    create models

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,21 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    categoryName = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.categoryName
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+    price = models.FloatField()
+    imageUrl = models.CharField(max_length=1000)
+    isActive = models.BooleanField(default=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name="user")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name="category")
+
+    def __str__(self):
+        return self.title
